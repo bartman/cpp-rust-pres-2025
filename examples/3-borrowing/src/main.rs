eafc949b1a2f27bd229c0958446f5498c1f89ce4
Bart Trojanowski <bart@jukie.net>
2025-09-25 14:55:12 -0400

    update example

--- examples/3-borrowing/src/main.rs
+++ examples/3-borrowing/src/main.rs
@@ -1,6 +1,5 @@
-fn thief(s: String) {
-    println!("thief: {}", s);
-
+fn thief(t: String) {
+    println!("thief: {}", t);
 }
 
 fn main() {
@@ -8,22 +7,24 @@ fn main() {
 
     println!("before: {}", s);
 
-    // transfer of ownership to the function
+    // transfer of ownership
     thief(s);
 
     // this will not compile...
-    // println!("before: {}", s);
+    println!("after: {}", s);
 }
 
 /*
 src/main.rs|13 col 28 error 382| borrow of moved value: `s`
 ||    |
-|| 7  |     let s = String::from("hello");
-||    |         - move occurs because `s` has type `String`, which does not implement the `Copy` trait
+|| 6  |     let s = String::from("hello");
+||    |         - move occurs because `s` has type `String`,
+||    |           which does not implement the `Copy` trait
 || ...
 || 11 |     thief(s);
 ||    |           - value moved here
-|| 12 |
-|| 13 |     println!("before: {}", s);
-||    |                            ^ value borrowed here after move
+|| ...
+|| 14 |     println!("after: {}", s);
+||    |                            ^ value borrowed here
+||    |                              after move
 */
